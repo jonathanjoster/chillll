c2dd0d55914805c5acf4bbe18509730cd835b5bc
jonathanjoster <jonathanjoster0331@gmail.com>
2023-06-22 14:35:59 +0900

    modify to descending first

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,6 @@
 module MoviesHelper
     def sort_order(column, title)
-        direction = (column == sort_column && sort_direction == 'asc') ? 'desc' : 'asc'
+        direction = (column == sort_column && sort_direction == 'desc') ? 'asc' : 'desc'
         link_to title, { sort: column, direction: direction }    
     end
 end
